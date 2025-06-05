a4d77335ddd5c44461f95f06d897b5871eff2526
sabil08 <khochak26@gmail.com>
2025-06-05 13:43:10 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,15 +2,16 @@
 
 PORT=${PORT:-3838}
 
-# Update config Shiny agar bind ke port $PORT
-echo "server {
-  listen $PORT;
+cat > /etc/shiny-server/shiny-server.conf <<EOF
+server {
+  listen ${PORT};
   location / {
     site_dir /srv/shiny-server;
     log_dir /var/log/shiny-server;
     directory_index on;
   }
-}" > /etc/shiny-server/shiny-server.conf
+}
+EOF
 
-echo "Starting Shiny on port $PORT ..."
+echo "Shiny Server running on port $PORT..."
 exec shiny-server
