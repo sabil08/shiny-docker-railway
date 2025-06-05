07646d530be57b9a45ca414d3c0ef7468073cff0
sabil08 <khochak26@gmail.com>
2025-06-05 13:23:00 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-# default port: 3838 jika PORT tidak diset oleh Railway
 PORT=${PORT:-3838}
 
-echo "Starting Shiny Server on port $PORT"
-/usr/bin/shiny-server
+# Update config Shiny agar bind ke port $PORT
+echo "server {
+  listen $PORT;
+  location / {
+    site_dir /srv/shiny-server;
+    log_dir /var/log/shiny-server;
+    directory_index on;
+  }
+}" > /etc/shiny-server/shiny-server.conf
+
+echo "Starting Shiny on port $PORT ..."
+exec shiny-server
