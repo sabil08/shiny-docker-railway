fd900c02612d75694a6afc58e2832a1f32a04f8c
sabil08 <khochak26@gmail.com>
2025-06-05 13:52:19 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,1 @@
-#!/bin/sh
 
-PORT=${PORT:-3838}
-
-cat > /etc/shiny-server/shiny-server.conf <<EOF
-server {
-  listen ${PORT};
-  location / {
-    site_dir /srv/shiny-server;
-    log_dir /var/log/shiny-server;
-    directory_index on;
-  }
-}
-EOF
-
-echo "Shiny Server running on port $PORT..."
-exec shiny-server
